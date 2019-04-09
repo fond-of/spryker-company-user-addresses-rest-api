e91a7be13060ff8ef7e82308b4c1859413d74dc9
Daniel Rose <daniel-rose@gmx.de>
2019-04-09 09:04:45 +0200

    init

---
+++ src/FondOfSpryker/Glue/CompanyUserAddressesRestApi/CompanyUserAddressesRestApiDependencyProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FondOfSpryker\Glue\CompanyUserAddressesRestApi;
+
+use Spryker\Glue\Kernel\AbstractBundleDependencyProvider;
+
+class CompanyUserAddressesRestApiDependencyProvider extends AbstractBundleDependencyProvider
+{
+}
