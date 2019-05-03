a69e8dc95219e9661775e2e07ef206c9df0e0184
Daniel Rose <daniel-rose@gmx.de>
2019-05-03 22:10:59 +0200

    add resource controller

---
+++ src/FondOfSpryker/Glue/CompanyUserAddressesRestApi/Controller/AddressesResourceController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FondOfSpryker\Glue\CompanyUserAddressesRestApi\Controller;
+
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+use Spryker\Glue\Kernel\Controller\AbstractController;
+
+/**
+ * @method \FondOfSpryker\Glue\CompanyUserAddressesRestApi\CompanyUserAddressesRestApiFactory getFactory()
+ */
+class AddressesResourceController extends AbstractController
+{
+    /**
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
+     */
+    public function getAction(RestRequestInterface $restRequest): RestResponseInterface
+    {
+        return $this->getFactory()->createCompanyUserAddressesReader()->getAddresses($restRequest);
+    }
+}
